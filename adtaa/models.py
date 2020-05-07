7c1971ae98ecde1d7d9cb23a7058fb906caf4342
Aaron Mays <aamays@ualr.edu>
2020-05-07 13:33:01 -0500

    Fixed the time labels

--- adtaa/models.py
+++ adtaa/models.py
@@ -20,7 +20,7 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=254, blank=True)
     class_load = models.IntegerField(validators=[validate_1to4])
-    areas = models.ManyToManyField(Area)
+    areas = models.ManyToManyField('Area', verbose_name='teacher areas')
 
     def __str__(self):
         return self.last_name
@@ -28,30 +28,31 @@ class Teacher(models.Model):
     def get_area_values(self):
         ret = ''
         print(self.areas.all())
-        # use models.ManyToMany field's all() method to return all the Department objects that this employee belongs to.
+        # use models.ManyToMany field's all() method
         for area in self.areas.all():
             ret = ret + area.name + ', '
         # remove the last ',' and return the value.
         return ret[:-2]
 
+
 class Course(models.Model):
     course_num = models.CharField(max_length=4, primary_key=True)
     title = models.CharField(max_length=200)
-    areas = models.ManyToManyField(Area)
+    areas = models.ManyToManyField(Area,  verbose_name='course areas')
     MONDAY = 'MW'
     TUESDAY = 'TH'
     MEET_DAY_CHOICES = [
         (MONDAY, 'Monday Wednesday'),
         (TUESDAY, 'Tuesday Thursday'),
     ]
-    AM800 = '8:00AM'
-    AM925 = '9:25AM'
-    AM1050 = '10:50AM'
-    PM1215 = '12:15PM'
-    PM140 = '1:40PM'
-    PM305 = '3:05PM'
-    PM430 = '4:30PM'
-    PM555 = '5:55PM'
+    AM800= '8:00AM-9:15AM'
+    AM925= '9:25AM-10:40AM'
+    AM1050= '10:50AM-12:05PM'
+    PM1215= '12:15PM-1:30PM'
+    PM140= '1:40PM-2:55PM'
+    PM305= '3:05PM-4:20PM'
+    PM430= '4:30PM-5:45PM'
+    PM555= '5:55PM-7:10PM'
     MEET_TIME_CHOICES = [
         (AM800, '8:00AM-9:15AM'),
         (AM925, '9:25AM-10:40AM'),
@@ -75,18 +76,7 @@ class Course(models.Model):
         for area in self.areas.all():
             ret = ret + area.name + ', '
         # remove the last ',' and return the value.
-        return ret[:-1]
-
-
-#class TeacherAreas(models.Model):
-#    last_name = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-#    area = models.ForeignKey(Area, on_delete=models.CASCADE)
-
-
-
-#class CourseAreas(models.Model):
-#    course_num = models.ForeignKey('Course', on_delete=models.CASCADE)
-#    area = models.ForeignKey('Area', on_delete=models.CASCADE)
+        return ret[:-2]
 
 
 class Schedule(models.Model):
@@ -100,14 +90,14 @@ class Schedule(models.Model):
         (MONDAY, 'Monday Wednesday'),
         (TUESDAY, 'Tuesday Thursday'),
     ]
-    AM800 = '8:00AM'
-    AM925 = '9:25AM'
-    AM1050 = '10:50AM'
-    PM1215 = '12:15PM'
-    PM140 = '1:40PM'
-    PM305 = '3:05PM'
-    PM430 = '4:30PM'
-    PM555 = '5:55PM'
+    AM800= '8:00AM-9:15AM'
+    AM925= '9:25AM-10:40AM'
+    AM1050= '10:50AM-12:05PM'
+    PM1215= '12:15PM-1:30PM'
+    PM140= '1:40PM-2:55PM'
+    PM305= '3:05PM-4:20PM'
+    PM430= '4:30PM-5:45PM'
+    PM555= '5:55PM-7:10PM'
     MEET_TIME_CHOICES = [
         (AM800, '8:00AM-9:15AM'),
         (AM925, '9:25AM-10:40AM'),
@@ -120,5 +110,3 @@ class Schedule(models.Model):
     ]
     meeting_day = models.CharField(max_length=2, choices=MEET_DAY_CHOICES)
     meeting_time = models.CharField(max_length=15, choices=MEET_TIME_CHOICES)
-
-# Create your models here.
